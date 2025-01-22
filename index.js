6c3834758d29c5053987c4f2ae27ce98f6a4c90d
r4478 <rk21eeb0a57@student.nitw.ac.in>
2025-01-22 18:14:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/insta',(req,res)=>{
+    res.send('RaviKumar_001')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at insta</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send("<h2>Chai aur Code</h2>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
